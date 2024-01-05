c317dc8a97f0c6c79fec751ca56f950f89d70c4b
Manvitha7075 <155752924+Manvitha7075@users.noreply.github.com>
2024-01-05 16:36:52 -0500

    Update RetrofitInstance.kt

--- app/src/main/java/com/example/countrieslist/retrofit/RetrofitInstance.kt
+++ app/src/main/java/com/example/countrieslist/retrofit/RetrofitInstance.kt
@@ -7,7 +7,6 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 class RetrofitInstance {
     companion object {
-        var ret:Int = 2
         private val retrofit: Retrofit by lazy {
             Retrofit.Builder().baseUrl("https://gist.githubusercontent.com/").addConverterFactory(GsonConverterFactory.create())
                 .build()
